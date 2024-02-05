9126a58fb302bebd799a4d92a6ad5cf1f01e4087
[sadra sadeghi] <[sadra82.sadeghi@gmail.com]>
2024-02-05 19:03:57 +0330

    user service done

--- controller/user.controller.ts
+++ controller/user.controller.ts
@@ -6,19 +6,9 @@ import { NextFunction, Request, Response } from "express"
 class UserController {
   private userService = new UserService()
 
-  public async getUsers(req: Request, res: Response, next: NextFunction) {
-    try {
-      const allUsers = await this.userService.findAllUser()
-      res.json({ data: allUsers })
-    } catch (error) {
-      console.log("error")
-      next(error)
-    }
-  }
-
   public async createUsers(req: Request, res: Response, next: NextFunction) {
     try {
-      const newUser = await this.userService.createUser(
+      const newUser = await this.userService.createUserService(
         req.body.username,
         req.body.password,
         req.body.email
@@ -26,6 +16,56 @@ class UserController {
       res.json({ data: newUser }).status(200)
     } catch (err) {
       console.log(err)
+      next(err)
+    }
+  }
+
+  public async getUsers(req: Request, res: Response, next: NextFunction) {
+    try {
+      const allUsers = await this.userService.findAllUserService()
+      res.json({ data: allUsers })
+    } catch (err) {
+      console.log(err)
+      next(err)
+    }
+  }
+
+  public async findOneUser(req: Request, res: Response, next: NextFunction) {
+    try {
+      const user = await this.userService.findOneUserService(
+        req.params.username
+      )
+      res.json({ user }).status(200)
+    } catch (err) {
+      console.log(err)
+      next(err)
+    }
+  }
+
+  public async updateUser(req: Request, res: Response, next: NextFunction) {
+    try {
+      const user = await this.userService.updateUserService(
+        req.body.username,
+        req.body.password,
+        req.body.email
+      )
+      res.json({ user }).status(200)
+    } catch (err) {
+      console.log(err)
+      next(err)
+    }
+  }
+
+  public async deleteUser(req: Request, res: Response, next: NextFunction) {
+    try {
+      const deletedUser = await this.userService.deleteUserService(
+        req.body.username,
+        req.body.password
+      )
+      res.json({ deletedUser }).status(200)
+    } catch (err) {
+      console.log(err)
+      next(err)
     }
   }
 }
--- service/user.service.ts
+++ service/user.service.ts
@@ -4,15 +4,37 @@ import userModel from "../models/user.model"
 
 class UserService {
   private userModel = userModel
-  public async findAllUser(): Promise<IUser[]> {
-    return await this.userModel.find()
-  }
-  public async createUser(username: string, password: string, email: string) {
+
+  public async createUserService(
+    username: string,
+    password: string,
+    email: string
+  ) {
     return await this.userModel.create({
       username,
       password,
       email,
     })
+  }
+
+  public async findAllUserService(): Promise<IUser[]> {
+    return await this.userModel.find()
+  }
+
+  public async findOneUserService(username: string) {
+    return await this.userModel.findOne({ username })
+  }
+
+  public async updateUserService(
+    username: string,
+    password: string,
+    email: string
+  ) {
+    return await this.userModel.updateMany({ username, password, email })
+  }
+
+  public async deleteUserService(username: string, password: string) {
+    return await this.userModel.deleteMany({ username, password })
   }
 }
 
